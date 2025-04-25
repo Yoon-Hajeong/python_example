cb4a5a6847a20f0c983d2ff49caae7353413709b
Yoon- Hajeong <pionksribboon@gmail.com>
2025-04-25 16:50:35 +0900

    todayclass

--- 250425/first.py
+++ 250425/first.py
@@ -4,6 +4,32 @@ print("뭘쳐다보니")
 
 
 
+print("* * * * * * * * * * *")
+print("*                   *")
+print("*                   *")
+print("*                   *")
+print("*                   *")
+print("* * * * * * * * * * *")
+
+
+
+
+name = "김치찌개"
+name = "김치볶음밥"
+
+a = 10; b = 20
+print(a, "+", a, "=", b)
+
+PI = 3.14
+print(PI)
+PI = 2
+print(PI)
+
+print(type(PI))
+print(type(name))
+
+
+
 
 
 
